990f31ee52450b516b81b87fc340104af839974c
LevanBokhua-dev <lbokhuadev@gmail.com>
2025-07-08 14:08:04 +0400

    permit api/**

--- src/main/java/com/task/yourcsguide/config/SecurityConfig.java
+++ src/main/java/com/task/yourcsguide/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults()) // 👈 Add this line
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
